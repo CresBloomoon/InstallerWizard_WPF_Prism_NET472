0b2f735b58016860457cd9e6985885373c3dd3f4
CresBloomoon <metroidm2421@gmail.com>
2023-05-28 21:59:15 +0900

    プロジェクト ファイルを追加します。

---
+++ InstallerWizard/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using InstallerWizard.Views;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InstallerWizard
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<WizardInputPage>();
+        }
+    }
+}
---
+++ InstallerWizard/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Regions;
+using System.Windows;
+
+namespace InstallerWizard.Views
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(IRegionManager regionManager)
+        {
+            InitializeComponent();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(WelcomePage));
+        }
+    }
+}
